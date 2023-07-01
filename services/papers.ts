c2a996ddf43260f2d7bcd82b4c164ea74742781b
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-07-02 01:59:41 +0200

    Update papers.ts

--- services/papers.ts
+++ services/papers.ts
@@ -6,9 +6,20 @@ import { buildMatchObject, fixYearData, quartilePosition } from '../utils/queryU
 
 export default () => {
   const models = require('../db/models');
-  const { paper: Papers } = models;
+  console.log("modeles: ", models)
+  const { paper: Papers, PaperAuthor, authorTable: Author } = models;
 
   return {
+    addPaper: async () => {
+      // const _paper = { "corpusid": 224735017, "externalids": { "ACL": null, "DBLP": null, "ArXiv": null, "MAG": "952784131", "CorpusId": "224735017", "PubMed": null, "DOI": null, "PubMedCentral": null }, "url": "https://www.semanticscholar.org/paper/97abd35eb7d004e70a58652f2fa762620ad0ea73", "title": "当“商业模式”嫁接“职业规划”", "authors": [{ "authorId": "82841146", "name": "杨吉" }], "venue": "", "publicationvenueid": null, "year": 2012, "referencecount": 0, "citationcount": 0, "influentialcitationcount": 0, "isopenaccess": false, "s2fieldsofstudy": null, "publicationtypes": null, "publicationdate": null, "journal": { "name": "", "pages": "96-96", "volume": "" }, "updated": "2022-01-27T01:47:09.787Z" };
+      // const uploadedPaper = await Papers.create({ id: "test2", authorIds: ["82841146"] })
+      const test = await Author.findAll()
+      const test2 = await Papers.findAll()
+      const test3 = await Papers.findAll({ include: Author })
+      const test4 = await Author.findAll({ include: Papers })
+      console.log("test: ", test, test2, test3, test4)
+      return { test, test2, test3, test4 }
+    },
     getPapers: async (req: Request<{}, {}, {}, QueryFilters>, res: Response) => {
       const matchObject = buildMatchObject(req.query)
       console.log("matchObject: ", matchObject);
