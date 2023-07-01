4c4d20e39210f497d14bc4a7379e50b14e2c6032
Muhammad Talha <32730920+muhammadtalha242@users.noreply.github.com>
2023-07-02 01:58:42 +0200

    Update Authors.js

--- db/models/Authors.js
+++ db/models/Authors.js
@@ -1,18 +1,41 @@
-// const Sequelize = require('sequelize');
 const { DataTypes } = require('sequelize');
-const Papers = require('./Papers')
 
 module.exports = (sequelize) => {
-  const Authors = sequelize.define('author', {
-    id: {
+  const Authors = sequelize.define('authorTable', {
+    authorid: {
       type: DataTypes.STRING,
       primaryKey: true,
       allowNull: false,
     },
-    affiliations: {
-      type: DataTypes.STRING,
-      allowNull: true,
+    externalids: {
+      type: DataTypes.JSON,
     },
+    url: {
+      type: DataTypes.STRING,
+    },
+    name: { type: DataTypes.STRING, },
+    aliases: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    affiliations: {
+      type: DataTypes.STRING
+    },
+    homepage: {
+      type: DataTypes.STRING
+    },
+    papercount: {
+      type: DataTypes.INTEGER
+    },
+    citationcount: {
+      type: DataTypes.INTEGER
+    },
+    hindex: {
+      type: DataTypes.INTEGER
+    },
+    updated: {
+      type: DataTypes.DATE,
+    }
+    ,
     createdAt: {
       type: DataTypes.DATE,
       allowNull: true,
@@ -21,34 +44,26 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    fullname: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    number: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
     orcid: {
       type: DataTypes.STRING,
       allowNull: true,
     },
   }, {
-    tableName: 'authors',
+    tableName: 'authorTable',
     timestamps: false,
   });
   // console.log("Papers: ", Papers);
   // Authors.belongsToMany(Papers, {through: "paper_author"})
 
-  // Authors.associate = (models) => {
-  //   // Add the many-to-many association with the papers model
-  //   console.log("Authors models: ", models);
+  Authors.associate = (models) => {
+    // Add the many-to-many association with the papers model
+    console.log("Authors models: ", models);
 
-  //   Authors.belongsToMany(models.paper, {
-  //     through: models.PaperAuthor,
-  //     foreignKey: 'authorId',
-  //   });
-  // };
+    Authors.belongsToMany(models.paper, {
+      through: "PaperAuthor",
+      foreignKey: 'authorId',
+    });
+  };
 
   return Authors
 }
