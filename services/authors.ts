bd1f108892ccc3cae807dab09b94fbc22e391c94
Muhammad Talha <m.talha2244@gmail.com>
2023-08-14 08:12:07 +0200

    Update authors.ts

--- services/authors.ts
+++ services/authors.ts
@@ -2,58 +2,12 @@ import { Request, Response } from 'express';
 import sequelize, { FindAndCountOptions, FindOptions } from 'sequelize';
 import { IAuthor, PagedParameters, QueryFilters, TopKParameters } from '../interfaces/types';
 import { buildMatchObject, buildSortObject, fixYearData, quartilePosition } from '../utils/queryUtil';
-import * as fs from 'fs';
+import { readAndLoadFile, splitArrayIntoSubArrays } from '../utils/fileReader';
 
 
 export default () => {
   const models = require('../db/models');
   const { paper: Papers, authorTable: Author } = models;
-
-  function splitArrayIntoSubArrays(largeArray) {
-    const subArraySize = Math.ceil(largeArray.length / 50); // Calculate size of each sub-array
-    const subArrays = [];
-
-    for (let i = 0; i < largeArray.length; i += subArraySize) {
-      subArrays.push(largeArray.slice(i, i + subArraySize));
-    }
-
-    return subArrays;
-  }
-
-  async function readAndLoadFile(filePath: string): Promise<any[]> {
-    const authorsArray = [];
-
-    return new Promise((resolve, reject) => {
-
-      const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
-      let jsonData = '';
-      let buffer = ""
-      readStream.on('data', (chunk) => {
-        buffer += chunk
-        jsonData = JSON.parse(JSON.stringify(buffer.toString()));
-        try {
-          const lines = jsonData.split("\n");
-          buffer = lines.pop();
-          const t = lines.map((l) => { return JSON.parse(l) })
-          authorsArray.push(...t);
-        } catch (error) {
-          reject(error);
-        }
-      });
-
-      readStream.on('end', () => {
-        try {
-          resolve(authorsArray);
-        } catch (error) {
-          reject(error);
-        }
-      });
-
-      readStream.on('error', (error) => {
-        reject(error);
-      });
-    });
-  }
 
   function checkValidity(data: string): boolean {
     try {
@@ -100,13 +54,17 @@ export default () => {
       } else {
         const authorId = "62bf285db8ab9b1341da1673"
         try {
+          const authorFull = await Author.findOne({
+            where: {
+              authorid: '103989795'
+            },
+            include: Papers
+
+          })
+          console.log("AuthorFull ", authorFull)
           const test = await Author.findAll({
             distinct: true,
-            include: [{
-              model: Papers,
-              on: sequelize.literal(`author.id = ANY("papers"."authorIds")`),
-
-            }],
+            include: Papers,
             limit: pageSize,
             // group: ['author.id'],
           })
@@ -174,7 +132,7 @@ export default () => {
           //     yearPublishedLast: row.dataValues.yearPublishedLast,
           //   })),
           // };
-          res.json({ test });
+          res.json({ authorFull, test });
         }
         catch (error: any) {
           res.status(500).json({ message: error.message });
