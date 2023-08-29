3c79d875b69148c1aba74a235d3b177959e718f5
Muhammad Talha <m.talha2244@gmail.com>
2023-08-29 20:03:02 +0200

    Update fileReader.ts

--- utils/fileReader.ts
+++ utils/fileReader.ts
@@ -1,4 +1,6 @@
 import * as fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
 
 export function splitArrayIntoSubArrays(largeArray) {
     const subArraySize = Math.ceil(largeArray.length / 50); // Calculate size of each sub-array
@@ -12,14 +14,18 @@ export function splitArrayIntoSubArrays(largeArray) {
 }
 
 export async function readAndLoadFile(filePath: string): Promise<any[]> {
-    const authorsArray = [];
 
+    const authorsArray = [];
+    const folderPath = './data/authors';
+
+    const decompressedFilePath = await decompressFile(filePath, folderPath)
     return new Promise((resolve, reject) => {
 
-        const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+        const readStream = fs.createReadStream(decompressedFilePath, { encoding: 'utf-8' })
         let jsonData = '';
         let buffer = ""
         readStream.on('data', (chunk) => {
+
             buffer += chunk
             jsonData = JSON.parse(JSON.stringify(buffer.toString()));
             try {
@@ -42,6 +48,32 @@ export async function readAndLoadFile(filePath: string): Promise<any[]> {
 
         readStream.on('error', (error) => {
             reject(error);
+        });
+    });
+
+}
+
+export async function decompressFile(
+    inputFilePath: string,
+    folderPath: string
+): Promise<string> {
+    const filePath = `${inputFilePath.split('/')[4].split('.')[0]}.txt`
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+    }
+    const outputFilePath = path.join(folderPath, filePath);
+    const readStream = fs.createReadStream(inputFilePath);
+    const decompressStream = zlib.createGunzip();
+    const writeStream = fs.createWriteStream(outputFilePath);
+
+    readStream.pipe(decompressStream).pipe(writeStream);
+
+    return new Promise((resolve, reject) => {
+        writeStream.on('finish', () => {
+            resolve(`${outputFilePath}`);
+        });
+        writeStream.on('error', (error) => {
+            reject(`Error during decompression: ${error.message}`);
         });
     });
 }
