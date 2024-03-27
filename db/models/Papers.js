9b4b6aea711d436397245e570d0e2934b548d45a
Muhammad Talha <m.talha2244@gmail.com>
2024-03-27 16:06:49 +0500

    Update Papers.js

--- db/models/Papers.js
+++ db/models/Papers.js
@@ -1,88 +1,91 @@
 // const Sequelize = require('sequelize');
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
-
-  const Papers = sequelize.define('paper', {
-    corpusid: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
+  const Papers = sequelize.define(
+    "paper",
+    {
+      corpusid: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      externalids: {
+        type: DataTypes.JSON,
+      },
+      authors: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        allowNull: true,
+      },
+      publicationvenueid: {
+        type: DataTypes.TEXT,
+      },
+      referencecount: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      s2fieldsofstudy: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        allowNull: true,
+      },
+      publicationtypes: {
+        type: DataTypes.ARRAY(DataTypes.TEXT),
+        allowNull: true,
+      },
+      journal: {
+        type: DataTypes.JSON,
+        allowNull: true,
+      },
+      influentialcitationcount: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      citationcount: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      venue: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      isopenaccess: {
+        type: DataTypes.BOOLEAN,
+      },
+      venueId: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      year: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      updated: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      publicationdate: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
     },
-    externalids:{
-      type:DataTypes.JSON,
-    },
-    authors: {
-      type: DataTypes.ARRAY(DataTypes.JSON),
-      allowNull: true,
-    },
-    publicationvenueid:{
-      type: DataTypes.TEXT
-    },
-    referencecount: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    s2fieldsofstudy: {
-      type: DataTypes.ARRAY(DataTypes.JSON),
-      allowNull: true,
-    },
-    publicationtypes: {
-      type: DataTypes.ARRAY(DataTypes.TEXT),
-      allowNull: true,
-    },
-    journal: {
-      type: DataTypes.JSON,
-      allowNull: true,
-    },
-    influentialcitationcount: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    citationcount: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    url: {
-      type:DataTypes.TEXT,
-      allowNull: true,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    venue: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    isopenaccess:{
-      type: DataTypes.BOOLEAN
-    },
-    venueId: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    year: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    updated: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    publicationdate: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-  }, {
-    tableName: 'papers',
-    timestamps: false,
-  });
+    {
+      tableName: "papers",
+      timestamps: false,
+    }
+  );
 
   Papers.associate = (models) => {
     Papers.belongsToMany(models.authorTable, {
       through: "PaperAuthor",
-      foreignKey: 'paperId',
+      foreignKey: "paperId",
     });
   };
-  return Papers
-}
+  return Papers;
+};
