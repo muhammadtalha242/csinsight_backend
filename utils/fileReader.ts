5e4fb87268f6f932728ef25cfe64a775182b0704
Muhammad Talha <m.talha2244@gmail.com>
2024-03-27 16:07:34 +0500

    Update fileReader.ts

--- utils/fileReader.ts
+++ utils/fileReader.ts
@@ -1,79 +1,85 @@
-import * as fs from 'fs';
-import zlib from 'zlib';
-import path from 'path';
+import * as fs from "fs";
+import zlib from "zlib";
+import path from "path";
 
 export function splitArrayIntoSubArrays(largeArray) {
-    const subArraySize = Math.ceil(largeArray.length / 50); // Calculate size of each sub-array
-    const subArrays = [];
+  const subArraySize = Math.ceil(largeArray.length / 50); // Calculate size of each sub-array
+  const subArrays = [];
 
-    for (let i = 0; i < largeArray.length; i += subArraySize) {
-        subArrays.push(largeArray.slice(i, i + subArraySize));
-    }
+  for (let i = 0; i < largeArray.length; i += subArraySize) {
+    subArrays.push(largeArray.slice(i, i + subArraySize));
+  }
 
-    return subArrays;
+  return subArrays;
 }
 
-export async function readAndLoadFile(filePath: string): Promise<any[]> {
+export async function readAndLoadFile(
+  filePath: string,
+  folderPath: string
+): Promise<any[]> {
+  const authorsArray = [];
 
-    const authorsArray = [];
-    const folderPath = './data/authors';
+  //   const decompressedFilePath = await decompressFile(filePath, folderPath);
+  return new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(filePath, {
+      encoding: "utf-8",
+    });
+    let jsonData = "";
+    let buffer = "";
+    readStream.on("data", (chunk) => {
+      console.log("chunk: ", chunk);
 
-    const decompressedFilePath = await decompressFile(filePath, folderPath)
-    return new Promise((resolve, reject) => {
+      buffer += chunk;
+      jsonData = JSON.parse(JSON.stringify(buffer.toString()));
+      console.log("jsonData", jsonData);
 
-        const readStream = fs.createReadStream(decompressedFilePath, { encoding: 'utf-8' })
-        let jsonData = '';
-        let buffer = ""
-        readStream.on('data', (chunk) => {
-
-            buffer += chunk
-            jsonData = JSON.parse(JSON.stringify(buffer.toString()));
-            try {
-                const lines = jsonData.split("\n");
-                buffer = lines.pop();
-                const t = lines.map((l) => { return JSON.parse(l) })
-                authorsArray.push(...t);
-            } catch (error) {
-                reject(error);
-            }
+      try {
+        const lines = jsonData.split("\n");
+        buffer = lines.pop();
+        const t = lines.map((l) => {
+          return JSON.parse(l);
         });
-
-        readStream.on('end', () => {
-            try {
-                resolve(authorsArray);
-            } catch (error) {
-                reject(error);
-            }
-        });
-
-        readStream.on('error', (error) => {
-            reject(error);
-        });
+        authorsArray.push(...t);
+      } catch (error) {
+        reject(error);
+      }
     });
 
+    readStream.on("end", () => {
+      try {
+        resolve(authorsArray);
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    readStream.on("error", (error) => {
+      reject(error);
+    });
+  });
 }
 
 export async function decompressFile(
-    inputFilePath: string,
-    folderPath: string
+  inputFilePath: string,
+  folderPath: string
 ): Promise<string> {
-    const filePath = `${inputFilePath.split('/')[4].split('.')[0]}.txt`
-    if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath);
-    }
-    const outputFilePath = path.join(folderPath, filePath);
-    const readStream = fs.createReadStream(inputFilePath);
-    const decompressStream = zlib.createGunzip();
-    const writeStream = fs.createWriteStream(outputFilePath);
+  const filePath = `${inputFilePath.split("/")[4].split(".")[0]}.txt`;
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+  }
+  const outputFilePath = path.join(folderPath, filePath);
+  const readStream = fs.createReadStream(inputFilePath);
+  const decompressStream = zlib.createGunzip();
+  const writeStream = fs.createWriteStream(outputFilePath);
 
-    readStream.pipe(decompressStream).pipe(writeStream);
+  readStream.pipe(decompressStream).pipe(writeStream);
 
-    return new Promise((resolve, reject) => {
-        writeStream.on('finish', () => {
-            resolve(`${outputFilePath}`);
-        });
-        writeStream.on('error', (error) => {
-            reject(`Error during decompression: ${error.message}`);
-        });
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", () => {
+      resolve(`${outputFilePath}`);
     });
+    writeStream.on("error", (error) => {
+      reject(`Error during decompression: ${error.message}`);
+    });
+  });
 }
