39b9166d26ff6e4ecf55b07ac8526bd2ec512666
Muhammad Talha <m.talha2244@gmail.com>
2024-03-27 16:06:25 +0500

    Update paper.ts

--- controllers/paper.ts
+++ controllers/paper.ts
@@ -1,32 +1,46 @@
-import { Request, Response } from 'express';
-import papers from '../services/papers';
-import { PagedParameters, QueryFilters, TopKParameters } from '../interfaces/types';
+import { Request, Response } from "express";
+import papers from "../services/papers";
+import {
+  PagedParameters,
+  QueryFilters,
+  TopKParameters,
+} from "../interfaces/types";
 
 export default () => {
-    const paperService = papers();
-    return {
-        addPapers: async (req: Request, res: Response) => {
-            const data = await paperService.addPapers(req, res);
-            res.json(data);
-        },
-        getPapers: async (req: Request, res: Response) => {
-            const data = await paperService.getPapers(req, res);
-            res.json(data);
-        },
-        getPapersInfo: async (req: Request<{}, {}, {}, QueryFilters & PagedParameters>, res: Response) => {
-            const data = await paperService.getPaperInfo(req, res);
-            res.json(data);
-        },
-        getPapersTopk: async (req: Request<{}, {}, {}, QueryFilters & TopKParameters>,
-            res: Response) => {
-            const data = await paperService.getPaperTopk(req, res);
-            res.json(data);
-        },
-        getPapersQuartiles: async (req: Request<{}, {}, {}, QueryFilters>,
-            res: Response) => {
-            const data = await paperService.getPaperQuartiles(req, res);
-            res.json(data);
-        }
-    }
-
-}
+  const paperService = papers();
+  return {
+    addPapers: async (req: Request, res: Response) => {
+      const data = await paperService.addPapers(req, res);
+      res.json(data);
+    },
+    getPapers: async (req: Request, res: Response) => {
+      const data = await paperService.getPapers(req, res);
+      res.json(data);
+    },
+    getPapersPost: async (req: Request, res: Response) => {
+      const data = await paperService.getPapersPost(req, res);
+      res.json(data);
+    },
+    getPapersInfo: async (
+      req: Request<{}, {}, {}, QueryFilters & PagedParameters>,
+      res: Response
+    ) => {
+      const data = await paperService.getPaperInfo(req, res);
+      res.json(data);
+    },
+    getPapersTopk: async (
+      req: Request<{}, {}, {}, QueryFilters & TopKParameters>,
+      res: Response
+    ) => {
+      const data = await paperService.getPaperTopk(req, res);
+      res.json(data);
+    },
+    getPapersQuartiles: async (
+      req: Request<{}, {}, {}, QueryFilters>,
+      res: Response
+    ) => {
+      const data = await paperService.getPaperQuartiles(req, res);
+      res.json(data);
+    },
+  };
+};
