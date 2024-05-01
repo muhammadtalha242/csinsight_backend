0fd9c1a9cc2d341f4b0ad3eb03121461b3e4adcd
Muhammad Talha <m.talha2244@gmail.com>
2024-05-01 21:31:54 +0200

    Update redis.ts

--- redis.ts
+++ redis.ts
@@ -1,3 +1,24 @@
+// import Redis from 'ioredis';
+
+// // Use environment variable for Redis host, default to 'localhost' if not set
+// const redisHost = process.env.REDIS_HOST || 'localhost';
+// const redisPort = parseInt(process.env.REDIS_PORT) || 6379; // Default Redis port
+
+// // Connect to Redis using the environment variable
+// const redisClient = new Redis(
+//     redisPort,
+//     redisHost
+// );
+
+// redisClient.on('connect', () => {
+//     console.log('Connected to Redis');
+// });
+
+// redisClient.on('error', (err) => {
+//     console.error('Redis connection error:', err);
+// });
+
+// export default redisClient;
 // redisClient.ts
 import Redis from "ioredis";
 
